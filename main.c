8bad41f58a407a256e3e1fefbcf3ec566fc463a7
rockatoo <youlia73@naver.com>
2019-03-20 10:00:01 +0900

    Emit light following timeline as cosin
    Signed-off-by: rockatoo <youlia73@naver.com>

--- main.c
+++ main.c
@@ -8,6 +8,10 @@
 #define LED1_T	GpioDataRegs.GPATOGGLE.bit.GPIO25 = 1	//GPIO25 핀 현재값에 반전
 #define LED2_T	GpioDataRegs.GPATOGGLE.bit.GPIO24 = 1	//GPIO24 핀 현재값에 반전
 
+#define DELAY_MAX 1000000
+#define DELAY_MIN 0
+#define DELAY_ALPHA 100000
+
 void Gpio_select(void);
 
 void main(){
@@ -21,24 +25,26 @@ void main(){
 	LED1_L;
 	LED2_L;		//LED OFF(초기화)
 
-	for(;;)
-	{
-		LED1_T;
-		LED2_T;				//LED ON/OFF
-		DELAY_US(200000);	//1초 지연
+	double delay, alpha;
+	for(delay = DELAY_MAX, alpha = -DELAY_ALPHA;; delay += alpha){
+		LED1_T; LED2_T;
+		DELAY_US(delay);
+
+		if(delay > DELAY_MAX || delay < DELAY_MIN)
+			alpha *= -1;
 	}
 }
 
 void Gpio_select(void)
 {
-   EALLOW;
+   EALLOW; // 보호 해제(레지스터를 사용할 때 보호를 풀었다가 다시 아요해주어야함)
 	GpioCtrlRegs.GPAMUX2.all = 0; //GPIO16...gpio31
 
 
 	GpioCtrlRegs.GPADIR.bit.GPIO25 = 1;	//OUTPUT 설정
 	GpioCtrlRegs.GPADIR.bit.GPIO24 = 1;	//OUTPUT 설정
 	
-   EDIS;
+   EDIS; // 보호 사용
 
 }
 
